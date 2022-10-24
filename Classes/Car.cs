2d168ba1c773339b5af9597b934791b8c34651c3
David Figone <david.louis.figone@gmail.com>
2022-10-24 07:03:25 -0500

    initial commit

---
+++ Classes/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string makeInput, string modelInput, int year)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    
+    
+    }
+
+    
+}
